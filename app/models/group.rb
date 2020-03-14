cd1a08390f98c2ec50e5ec596a3d1a0a9d1298aa
Mohamed Naser <nc_m.naser@hotmail.com>
2020-03-14 16:00:55 +0200

    add validation rules for (user , transaction, group) models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
     has_many :transactions
 
+    validates_length_of :name, within: 3..150, message: 'Must have a valid group name from 3 to 150 chars'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    
+  validates_length_of :name, within: 3..150, message: 'must have a valid length (3 , 150) chars'
 end
